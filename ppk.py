1cefb421d4fcc4b32bd5d04964efcc8149953821
pattorgod <pattorgod@github.com>
2020-07-24 20:01:02 +0200

    still testing

--- ppk.py
+++ ppk.py
@@ -10,7 +10,7 @@ import os
 import time
 import argparse
 
-Index0Url = str("hhttps://raw.githubusercontent.com/GNU-Pattor-Team/PPK-Sources-Main/master/main.index")
+Index0Url = str("https://raw.githubusercontent.com/GNU-Pattor-Team/PPK-Sources-Main/master/main.index")
 Index1Url = str("")
 
 ap = argparse.ArgumentParser()
@@ -36,10 +36,29 @@ def printHelp():
 def printTerms():
 	print("\nPPK (Pattor Packager) | A gateway to everything pirate on GNU/Linux.\nCopyright (C) 2020, GNU/Pattor Team\n\n<web>\n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License,\nor (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n")
 
+def search_string_in_file(text_file_name, string_to_search):
+	current_index = 0
+	line_number = 0
+	list_of_results = []
+	with open(text_file_name, "r") as read_obj:
+		for line in read_obj:
+			line_number += 1
+			if string_to_search in line:
+				list_of_results.append((line_number, line.rstrip()))
+	return list_of_results
+
 def ppkFetch():
 	os.system("mkdir /opt/ppk/lists/ && mkdir /opt/ppk/packages/")
 	print("\nFetching latest sources index...")
 	os.system("wget --show-progress -O /opt/ppk/lists/main.index " + Index0Url)
+	text_file_name = str("/opt/ppk/lists/main.index")
+	line_number = 0
+	with open(text_file_name) as f:
+		for line in f:
+			os.system("wget --show-progress -O /opt/ppk/lists/sources.index" + str(line_number) + " " + line)
+			line_number += 1
+	f.close()
+	print("\nDone fetching sources.")
 
 def ppkList():
 	ppkFetch()
@@ -80,5 +99,4 @@ if (str(args.remove) != "None"):
 
 if (str(args.purge) != "None"):
 	ppkPurge()
-
 
